ba113fc684adf77534bd3eba232f9d299a05ab87
Alex Borecký <boreckyalex@gmail.com>
2025-10-15 19:42:28 +0200

    Fix nodemailer typo and next.config.js warnings

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com'],
-  },
-  env: {
-    GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
-    LEADINFO_ID: process.env.LEADINFO_ID,
   },
 }
 
